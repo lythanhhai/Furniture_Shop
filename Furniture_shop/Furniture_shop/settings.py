aaae83cdbe4044f28cdaecbcb8e2af324a49ae64
BINH <77979118+rapmons@users.noreply.github.com>
2022-04-24 17:01:23 +0700

    create backend folder

--- Furniture_shop/Furniture_shop/settings.py
+++ Furniture_shop/Furniture_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8$d8g)2auw7z28uvz)g@x*zy83hqy=#aon9o=ka2_ku69mi5hg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
