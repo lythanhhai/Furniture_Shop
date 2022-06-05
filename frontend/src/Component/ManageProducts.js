0967a73cfaa90d2c2f22d8deef2d06722a7754e8
lythanhhai <lythanhhait@gmail.com>
2022-06-05 17:30:44 +0700

    post and get product for admin

--- frontend/src/Component/ManageProducts.js
+++ frontend/src/Component/ManageProducts.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import ModalAddProduct from './ModalAddProduct'
 import { useSelector, useDispatch } from 'react-redux'
 import { showModalProduct, hideModalProduct } from '../Action/showModalProduct'
-// import axios from 'axios'
+import axios from 'axios'
 
 const ManageProducts = () => 
 {
@@ -17,21 +17,21 @@ const ManageProducts = () =>
         dispatch(hideModalProduct())
     }
     // const [countClick, setCountClick] = useState(0) 
-    // const [departments, setDepartment] = useState([])
+    const [products, setProducts] = useState([])
 
-    // const getDepartment = () => {
-    //     axios.get('http://localhost:8000/Department/')
-    //     .then(response => response.data)
-    //     .then(data => {
-    //         //console.log(data); 
-    //         setDepartment(data)
-    //     })
-    //     .catch(err => {alert(err)})
-    // }
+    const getProducts = () => {
+        axios.get('http://127.0.0.1:8000/sale/Product-list/')
+        .then(response => response.data)
+        .then(data => {
 
-    // useEffect(() => {
-    //     getDepartment()
-    // },[])
+            setProducts(data)
+        })
+        .catch(err => {alert(err)})
+    }
+
+    useEffect(() => {
+        getProducts()
+    },[])
 
     // const handleAdd = () => {
     //     if(countClick === 0)
@@ -58,23 +58,26 @@ const ManageProducts = () =>
     //     .catch(err => {console.log(err)})
     // }
 
-    // const dataDepartmentElement = departments.map((department, index) => {
-    //     const {DepartmentId, DepartmentName} = department
-    //     return(
-    //             <tr key={index}>
-    //                 <td className='data'>{DepartmentId}</td>
-    //                 <td className='data'>{DepartmentName}</td>
-    //                 <td className='data'>
-    //                     <button className='edit'>
-    //                         Edit
-    //                     </button>
-    //                     <button className='delete' onClick={() => {handleDelete(index)}}>
-    //                         Delete
-    //                     </button>
-    //                 </td>
-    //             </tr>
-    //     );
-    // })
+    const dataProductElement = products.map((product, index) => {
+        const {id, name_product, price, desc, url} = product
+        return(
+                <tr key={index}>
+                    <td className='data'>{id}</td>
+                    <td className='data'>{name_product}</td>
+                    <td className='data'>{desc}</td>
+                    <td className='data'>{price}</td>
+                    <td className='data'>{url}</td>
+                    <td className='data'>
+                        <button className='edit'>
+                            Edit
+                        </button>
+                        <button className='delete' onClick={() => {}}>
+                            Delete
+                        </button>
+                    </td>
+                </tr>
+        );
+    })
     return(
         <>
             <section className='ManageProducts' onClick={() => {}}>
@@ -90,7 +93,7 @@ const ManageProducts = () =>
                         </tr>
                     </thead>
                     <tbody>
-
+                            {dataProductElement}
                     </tbody>
                 </table>
                 <button type='button' className='Button' onClick={() => {
--- frontend/src/Component/ModalAddProduct.js
+++ frontend/src/Component/ModalAddProduct.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import '../Asset/ModalAddProduct/ModalAddProduct.scss'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import { useSelector, useDispatch } from 'react-redux'
 import { showModalProduct, hideModalProduct } from '../Action/showModalProduct'
 
 const ModalAddProduct = () => {
     const [productName, setProductName] = useState({
-
+        name_product: "",
+        price: 0,
+        desc: "",
+        url: ""
     })
+
+    const form = useRef(null)
+
+    const [selectedFile, setSelectedFile] = useState()
+    const [preview, setPreview] = useState()
+
     const showModalAdd = useSelector(state => state.showModalProductReducer).value
     const dispatch = useDispatch()
     const handleShowModalAdd = () => {
@@ -17,23 +26,40 @@ const ModalAddProduct = () => {
     const handleHideModalAdd = () => {
         dispatch(hideModalProduct())
     }
-    // const handleAddDepartment = () => {
-    //     const Department = {
-    //         DepartmentName: departmentName,
-    //     }
-    //     axios.post(`http://localhost:8000/Department/`, Department)
-    //     .then(res => {
-    //         console.log(res);
-    //         console.log(res.data);
-    //     })
-    // }
-    const [selectedFile, setSelectedFile] = useState()
-    const [preview, setPreview] = useState()
+    // add product
+    const handleAddProduct = (e) => {
+        e.preventDefault()
+        const product = {
+            ...productName
+        };
+        var bodyFormData = new FormData();
+        bodyFormData.append('name_product', product["name_product"]);
+        bodyFormData.append('price', product["price"]);
+        bodyFormData.append('desc', product["desc"]);
+        bodyFormData.append('url', selectedFile); 
+        axios({
+            method: "POST",
+            url: "http://127.0.0.1:8000/sale/Product-create/",
+            data: bodyFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+          }).then(res => {
+              //handle success
+              console.log(res);
+            }).catch(err => {
+              //handle error
+              console.log(err);
+            });
+    }
+    
     useEffect(() => {
         if (!selectedFile) {
             setPreview(undefined)
             return
         }
+        setProductName({
+            ...productName,
+            url: selectedFile
+        })
 
         const objectUrl = URL.createObjectURL(selectedFile)
         setPreview(objectUrl)
@@ -41,6 +67,11 @@ const ModalAddProduct = () => {
         // free memory when ever this component is unmounted
         return () => URL.revokeObjectURL(objectUrl)
     }, [selectedFile])
+
+    useEffect(() => {
+        
+        // console.log(productName)
+    }, [productName])
 
     const onSelectFile = e => {
         if (!e.target.files || e.target.files.length === 0) {
@@ -57,19 +88,28 @@ const ModalAddProduct = () => {
                     <p onClick={() => {handleHideModalAdd()}}>close</p>
                 </div>
                 <hr></hr>
-                <form className='Input__Name' onSubmit={() => {}} method="POST">
+                <form className='Input__Name' ref={form} onSubmit={(e) => {handleAddProduct(e)}}>
                     <div className='Input__Name-left'>
                         <label htmlFor='product__name' id="label_name">Product Name: </label>
                         <input name="product__name" id="input_name" placeholder="Enter Product Name" onChange={
-                            (e) => {setProductName(e.target.value)}
+                            (e) => {setProductName({
+                                ...productName,
+                                name_product: e.target.value
+                            })}
                             }></input>
                         <label htmlFor='product__desc' id="label_description">Description: </label>
                         <input name="product__desc" id="input_description" placeholder="Enter Description" onChange={
-                            (e) => {setProductName(e.target.value)}
+                            (e) => {setProductName({
+                                ...productName,
+                                desc: e.target.value
+                            })}
                             }></input>
                         <label htmlFor='product__price' id="label_price">Price: </label>
                         <input name="product__price" id="input_price" placeholder="Enter Price" onChange={
-                            (e) => {setProductName(e.target.value)}
+                            (e) => {setProductName({
+                                ...productName,
+                                price: parseFloat(e.target.value)
+                            })}
                             }></input>
                         <button type='submit'>Add</button>
                     </div>
@@ -77,7 +117,7 @@ const ModalAddProduct = () => {
                        
                         {selectedFile !== undefined ? <img src={preview} /> : <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png" alt="err"></img> }
                         <input type='file' id="input_file" onChange={
-                            (e) => {onSelectFile(e); console.log(selectedFile)}
+                            (e) => {onSelectFile(e);}
                             }></input>
                     </div>
                 </form>
